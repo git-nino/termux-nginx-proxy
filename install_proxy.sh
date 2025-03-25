5e5b377d3b4ed005ff0003026ccf37532f689bc3
git-nino <alininozinou@gmail.com>
2025-03-25 11:01:58 +0100

    Removed untracked files

--- install_proxy.sh
+++ install_proxy.sh
@@ -10,20 +10,41 @@ mkdir -p $PREFIX/etc/nginx/sites-enabled
 
 cat > $PREFIX/etc/nginx/sites-enabled/reverse-proxy.conf <<EOF
 server {
-    listen 8080;
-    server_name 10.10.10.187;
-
+    listen 8001;
+    server_name _;
+    
     location / {
         proxy_pass http://192.168.1.1:80/;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-
-        # Fix incorrect redirects from backend
-#        proxy_redirect http://192.168.200.100/ http://192.168.200.1/;
-        proxy_redirect http://10.10.10.187/ http://192.168.1.1/;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     }
 }
+
+server {
+    listen 8100;
+    server_name _;
+    
+    location / {
+        proxy_pass http://192.168.100.1:80/;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    }
+}
+
+server {
+    listen 8200;
+    server_name _;
+    
+    location / {
+        proxy_pass http://192.168.200.1:80/;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    }
+}
+
 EOF
 
 sed -i '/http {/a \    server_names_hash_bucket_size 64;' $PREFIX/etc/nginx/nginx.conf
