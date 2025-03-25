44b31156f857ed8a7ee9339e396ac6aaa7f2af43
git-nino <alininozinou@gmail.com>
2025-03-25 11:31:11 +0100

    11

--- install_proxy.sh
+++ install_proxy.sh
@@ -10,41 +10,19 @@ mkdir -p $PREFIX/etc/nginx/sites-enabled
 
 cat > $PREFIX/etc/nginx/sites-enabled/reverse-proxy.conf <<EOF
 server {
-    listen 8001;
-    server_name _;
-    
-    location / {
-        proxy_pass http://192.168.1.1:80/;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    }
-}
+    listen 8000;
+    server_name 10.10.10.132;
 
-server {
-    listen 8100;
-    server_name _;
-    
-    location / {
-        proxy_pass http://192.168.100.1:80/;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    }
-}
-
-server {
-    listen 8200;
-    server_name _;
-    
     location / {
         proxy_pass http://192.168.200.1:80/;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+
+        # Fix incorrect redirects from backend
+        proxy_redirect http://10.10.10.132/ http://192.168.200.1/;
     }
 }
-
 EOF
 
 sed -i '/http {/a \    server_names_hash_bucket_size 64;' $PREFIX/etc/nginx/nginx.conf
@@ -57,4 +35,3 @@ nginx -s stop
 
 
 nginx
-
