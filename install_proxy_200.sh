cfff5c078b27af9c477d61780b81ff4d2a4a897f
git-nino <alininozinou@gmail.com>
2025-03-26 09:10:51 +0100

    the final update

--- install_proxy_200.sh
+++ install_proxy_200.sh
@@ -1,37 +1,44 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-
+# Update and upgrade packages
 yes | pkg update -y && yes | pkg upgrade -y
 
+# Install Nginx
 pkg install nginx -y
 
-
+# Create necessary directories
 mkdir -p $PREFIX/etc/nginx/sites-enabled
 
+# Create reverse proxy configuration
 cat > $PREFIX/etc/nginx/sites-enabled/reverse-proxy.conf <<EOF
 server {
-    listen 8080;
-    server_name 10.10.10.132;
+    listen 8282;
+    server_name _;  # Accepts requests on any IP
 
     location / {
         proxy_pass http://192.168.200.1:80/;
-        proxy_set_header Host \$host;
+        proxy_set_header Host 192.168.200.1;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 
-        # Fix incorrect redirects from backend
-        proxy_redirect http://10.10.10.132/ http://192.168.200.1/;
+        # Fix redirects from backend dynamically
+        proxy_redirect http://192.168.200.1/ http://\$host:8282/;
+
+        # Replace hardcoded URLs inside HTML responses dynamically
+        sub_filter 'href="http://192.168.200.1/' 'href="http://\$host:8282/';
+        sub_filter 'src="http://192.168.200.1/' 'src="http://\$host:8282/';
+        sub_filter_once off;
     }
 }
 EOF
 
+# Modify Nginx configuration
 sed -i '/http {/a \    server_names_hash_bucket_size 64;' $PREFIX/etc/nginx/nginx.conf
 
+# Ensure the sites-enabled directory is included
 grep -q "include $PREFIX/etc/nginx/sites-enabled/*;" $PREFIX/etc/nginx/nginx.conf || \
 sed -i '/http *{/a\    include '"$PREFIX"'/etc/nginx/sites-enabled/*;' $PREFIX/etc/nginx/nginx.conf
 
-
-nginx -s stop
-
-
+# Restart Nginx
+pkill nginx
 nginx
